e6e3a1fe1ca4481a5dd5dbacadf4b798fbcf8816
yaobin <bin.yao@auto-tech.net>
2018-04-05 16:08:33 +0800

    4.5.1

--- LFZB_PMS/Class/Config.cs
+++ LFZB_PMS/Class/Config.cs
@@ -12,5 +12,13 @@ namespace LFZB_PMS
         {
             public static string Server = "Server=106.14.176.19;Database=lfzb_data;User=lfzb;Password=1234;";
         }
+
+        public class XTSZ
+        {
+            /// <summary>
+            /// 默认显示单据天数
+            /// </summary>
+            public static int DaysOfOrder;
+        }
     }
 }
